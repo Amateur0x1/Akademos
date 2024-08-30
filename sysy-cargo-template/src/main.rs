51d523a0e1cae5f9463a8cfb2630d6aa23db0d08
Amateur0x1 <amateur11m11d@gmail.com>
2024-08-30 10:05:30 +0800

    feat: first commit

---
+++ sysy-cargo-template/src/main.rs
@@ -0,0 +1,37 @@
+
+use lalrpop_util::lalrpop_mod;
+use std::env::args;
+use std::io::Result;
+use std::fs::{read_to_string, write};
+use ir::{GenerateAsm};
+// 引用 lalrpop 生成的解析器
+// 因为我们刚刚创建了 sysy.lalrpop, 所以模块名是 sysy
+lalrpop_mod!(sysy);
+pub mod ast_module;
+
+mod ir;
+
+
+fn main() -> Result<()> {
+  // 解析命令行参数
+  let mut args = args();
+  args.next();
+  let _mode = args.next().unwrap();
+  let input = args.next().unwrap();
+  args.next();
+  let output = args.next().unwrap();
+
+  // 读取输入文件
+  let input = read_to_string(input)?;
+  // parse input file
+
+  let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+  dbg!(&ast);
+  // let koopa_code = ast.fmt().unwrap();
+  // // 写入输出文件
+  // let driver = koopa::front::Driver::from(koopa_code);
+  // let program = driver.generate_program().unwrap();
+  // let riscv_code = program.generate().unwrap();
+  // write(output, riscv_code);
+  Ok(())
+}
