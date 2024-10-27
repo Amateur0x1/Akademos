827c127600514d63fd5b62e9df67b0a0539ab598
Amateur <2863217986@qq.com>
2024-10-27 12:40:04 +0800

    1027

--- frontend/js/src/bind-call-apply.ts
+++ frontend/js/src/bind-call-apply.ts
@@ -1,3 +1,55 @@
+declare global {
+  interface Function {
+    myCall<T, U>(
+      this: (this: T, ...args: U[]) => any,
+      thisArg: T,
+      ...args: U[]
+    ): any;
+    myApply<T, U>(
+      this: (this: T, ...args: U[]) => any,
+      thisArg: T,
+      args?: U[]
+    ): any;
+    myBind<T, U>(
+      this: (this: T, ...args: U[]) => any,
+      thisArg: T,
+      ...args: U[]
+    ): (...newArgs: U[]) => any;
+  }
+}
+
+Function.prototype.myCall = function <T>(thisArg: T, ...args: any[]) {
+  
+  thisArg = (thisArg ?? globalThis) as T;
+  const fnSymbol = Symbol(); 
+  (thisArg as any)[fnSymbol] = this;
+
+  
+  const result = (thisArg as any)[fnSymbol](...args);
+
+  
+  delete (thisArg as any)[fnSymbol];
+  return result;
+};
+
+Function.prototype.myApply = function <T>(thisArg: T, args: any[] = []) {
+  thisArg = (thisArg ?? globalThis) as T;
+  const fnSymbol = Symbol();
+  (thisArg as any)[fnSymbol] = this;
+
+  const result = (thisArg as any)[fnSymbol](...args);
+  delete (thisArg as any)[fnSymbol];
+  return result;
+};
+
+Function.prototype.myBind = function (thisArg: any, ...args: any[]) {
+  const self = this; 
+  return function (...newArgs: any[]) {
+    return self.myCall(thisArg, ...args, ...newArgs); 
+  };
+};
+
+// Test
 import { expect } from 'chai';
 
 describe('Custom call, apply, and bind', function () {
@@ -18,7 +70,7 @@ describe('Custom call, apply, and bind', function () {
     }
 
     const result = add.myCall(obj, 1, 2);
-    expect(result).to.equal(45); // 42 + 1 + 2
+    expect(result).to.equal(45); 
   });
 
   it('should apply the function with myApply', function () {
@@ -27,7 +79,7 @@ describe('Custom call, apply, and bind', function () {
     }
 
     const result = add.myApply(obj, [1, 2]);
-    expect(result).to.equal(45); // 42 + 1 + 2
+    expect(result).to.equal(45); 
   });
 
   it('should bind the function with myBind', function () {
@@ -36,9 +88,9 @@ describe('Custom call, apply, and bind', function () {
     }
 
     const boundAdd = add.myBind(obj, 1);
-    const result = boundAdd(2);
+    const result = boundAdd(1);
 
-    expect(result).to.equal(45); // 42 + 1 + 2
+    expect(result).to.equal(45); 
   });
 
   it('should bind and maintain the correct context with myBind', function () {
--- frontend/js/src/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-declare interface Function {
-  myCall(context: any, ...args: any[]): any;
-  myApply(context: any, argsArray?: any[]): any;
-  myBind(context: any, ...bindArgs: any[]): (...args: any[]) => any;
-}
